8d0ce8337b34eaba928dd0ca391ebdc94b67ec8a
Mockingbird <you@example.com>
2018-08-05 00:53:02 +0300

    added media

--- article/urls.py
+++ article/urls.py
@@ -3,6 +3,7 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.static import serve
 
 urlpatterns = [
     url(r'^article/(?P<article_id>\d+)/$', views.article, name="post"),
@@ -10,8 +11,10 @@ urlpatterns = [
     url(r'^article/addcomment/(?P<article_id>\d+)/$', views.addcomment, name="comment"),
     url(r'^find/(\d+)/$', views.find_article, name="find"),
     url(r'^page/(\d+)/$', views.articles, name="post_with_pages"),
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
     url(r'^$', views.articles),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += staticfiles_urlpatterns()
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += staticfiles_urlpatterns()
